0c9786780d780536a366813022babf2774b719e1
repifanio <rafael.cepifanio@gmail.com>
2020-02-16 21:23:11 -0300

    Incluido arquivo de log na aplicação

--- src/main/java/com/camargo/salesmanagement/DemoApplication.java
+++ src/main/java/com/camargo/salesmanagement/DemoApplication.java
@@ -1,15 +1,23 @@
 package com.camargo.salesmanagement;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+// @EnableScheduling
 public class DemoApplication {
+
+	final static Logger logger = Logger.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Teste de app sem Schedulle");
+
+		if (logger.isDebugEnabled()) {
+			logger.error("This is debug");
+		}
 	}
 
 }
